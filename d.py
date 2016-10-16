9db37dabe6e437704c665e74474f4e4aecc2f4da
Sam Ruby <Wen@Wens-MacBook-Pro.local>
2016-10-17 00:04:55 +1100

    3

---
+++ d.py
@@ -0,0 +1,7 @@
+def createTeam(firstIndex, secondIndex, isRed,
+               first = 'DefenseAgent', second = 'AttackAgent'):
+
+    if self.red:
+     x = gameState.getWalls().width/4
+    else:
+     x = 3*gameState.getWalls().width/4
